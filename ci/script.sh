824d439ecf6303bc311968a71bd6b995d31bba67
Guillaume Charmetant <cguille.dev@gmail.com>
2019-01-21 13:59:49 +0100

    support sha512 hash algorithm

--- ci/script.sh
+++ ci/script.sh
@@ -13,11 +13,13 @@ main() {
 
     # Test the generate subcommand:
     cross run --target $TARGET generate 'password'
+    cross run --target $TARGET generate -a sha512 'password'
     cross run --target $TARGET generate --algorithm=sha256 'password'
 
     # Test the validate subcommand:
     cross run --target $TARGET validate --quiet '7HAPNn2PxK6dI6XaY3IDHELAjR/tac9pkgHaeQ0Txpvvk69D' 'password'
     cross run --target $TARGET validate --quiet -a sha256 '7HAPNn2PxK6dI6XaY3IDHELAjR/tac9pkgHaeQ0Txpvvk69D' 'password'
+    cross run --target $TARGET validate --quiet --algorithm=sha512 'ATe3LFxFP+US2SFBZ9sLKpeSNS+/ywgrYPFFLCuK28/+Ioz+jQPUegAgjaqHcxxumWnIywgOl8rGwTWQnOq/7i6TIps=' 'password'
 }
 
 # we don't run the "test phase" when doing deploys
--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() {
                         .help("The algorithm used to validate the given hash")
                         .takes_value(true)
                         .default_value("sha256")
-                        .possible_values(&["sha256"])
+                        .possible_values(&["sha256", "sha512"])
                         .required(true)
                 )
                 .arg(
@@ -54,7 +54,7 @@ fn main() {
                         .help("The algorithm used to generate the hash")
                         .takes_value(true)
                         .default_value("sha256")
-                        .possible_values(&["sha256"])
+                        .possible_values(&["sha256", "sha512"])
                         .required(true)
                 )
                 .arg(
@@ -129,6 +129,7 @@ fn generate(algorithm: Algorithm, password: &str) -> String {
 fn parse_algo(algo_name: &str) -> Algorithm {
     match algo_name {
         "sha256" => Algorithm::SHA256,
+        "sha512" => Algorithm::SHA512,
         _ => panic!("Unexpected algorithm {}", algo_name),
     }
 }
