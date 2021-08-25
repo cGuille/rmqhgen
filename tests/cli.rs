d39011e289adefae55aabda47a2d046c1c0c2127
Guillaume Charmetant <cguille.dev@gmail.com>
2021-08-25 13:59:18 +0200

    Add tests for generate subcommand

--- tests/cli.rs
+++ tests/cli.rs
@@ -44,7 +44,6 @@ fn test_help_generate() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-
 #[test]
 fn test_help_validate() -> Result<(), Box<dyn std::error::Error>> {
     let mut command = Command::cargo_bin("rmqhgen").unwrap();
@@ -54,6 +53,58 @@ fn test_help_validate() -> Result<(), Box<dyn std::error::Error>> {
     assert
         .success()
         .stdout(predicate::str::contains("USAGE:\n    rmqhgen validate"))
+        .stderr(predicate::str::is_empty());
+
+    Ok(())
+}
+
+#[test]
+fn test_generate_bare() -> Result<(), Box<dyn std::error::Error>> {
+    let mut command = Command::cargo_bin("rmqhgen").unwrap();
+
+    let assert = command.arg("generate").arg("some clear password").assert();
+
+    assert
+        .success()
+        .stdout(predicate::str::is_match("^.{48}\n$").unwrap())
+        .stderr(predicate::str::is_empty());
+
+    Ok(())
+}
+
+#[test]
+fn test_generate_sha512_short_algo() -> Result<(), Box<dyn std::error::Error>> {
+    let mut command = Command::cargo_bin("rmqhgen").unwrap();
+
+    let assert = command
+        .arg("generate")
+        .arg("-a")
+        .arg("sha512")
+        .arg("some clear password")
+        .assert();
+
+    assert
+        .success()
+        .stdout(predicate::str::is_match("^.{92}\n$").unwrap())
+        .stderr(predicate::str::is_empty());
+
+    Ok(())
+}
+
+#[test]
+fn test_generate_sha256_long_algo() -> Result<(), Box<dyn std::error::Error>> {
+    let mut command = Command::cargo_bin("rmqhgen").unwrap();
+
+    let assert = command
+        .arg("generate")
+        .arg("--algorithm")
+        .arg("sha256")
+        .arg("some clear password")
+        .assert();
+
+    assert
+        .success()
+        .stdout(predicate::str::is_match("^.{48}\n$").unwrap())
         .stderr(predicate::str::is_empty());
 
     Ok(())
