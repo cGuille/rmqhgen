1eed879828166d72c2d9cad34be3a8676d2d90ee
Guillaume Charmetant <cguille.dev@gmail.com>
2021-08-27 18:07:51 +0200

    Remove manuel tests from CI scripts
    The automated tests are now run with `cargo test`.

--- ci/script.sh
+++ ci/script.sh
@@ -10,17 +10,6 @@ main() {
     fi
 
     cross test --target $TARGET
-
-    # Test the generate subcommand:
-    cross run --target $TARGET generate 'password'
-    cross run --target $TARGET generate -a sha512 'password'
-    cross run --target $TARGET generate --algorithm=sha256 'password'
-
-    # Test the validate subcommand:
-    cross run --target $TARGET validate --quiet '7HAPNn2PxK6dI6XaY3IDHELAjR/tac9pkgHaeQ0Txpvvk69D' 'password'
-    cross run --target $TARGET validate --quiet -a sha256 '7HAPNn2PxK6dI6XaY3IDHELAjR/tac9pkgHaeQ0Txpvvk69D' 'password'
-    cross run --target $TARGET validate --quiet -a md5 'cH4ItNINimvBXIJPvL7cU5ZSxr8=' 'password'
-    cross run --target $TARGET validate --quiet --algorithm=sha512 'ATe3LFxFP+US2SFBZ9sLKpeSNS+/ywgrYPFFLCuK28/+Ioz+jQPUegAgjaqHcxxumWnIywgOl8rGwTWQnOq/7i6TIps=' 'password'
 }
 
 # we don't run the "test phase" when doing deploys
