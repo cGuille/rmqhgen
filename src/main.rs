a7a72620c42a99e24180fb032c792d54ada92261
Guillaume Charmetant <cguille.dev@gmail.com>
2022-07-29 21:21:12 +0200

    Update version to 0.2.5

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use sha2::Sha512;
 fn main() {
     let app_matches = App::new("RabbitMQ password hash generator")
         .setting(AppSettings::ArgRequiredElseHelp)
-        .version("0.2.4")
+        .version("0.2.5")
         .about("Generates and validates RabbitMQ compliant password hashes")
         .subcommand(
             SubCommand::with_name("validate")
